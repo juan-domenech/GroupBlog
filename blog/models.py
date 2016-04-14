064cc49e69cf1e7b44a5349dd237dc470c154218
mullaned <mullaned@gmail.com>
2016-04-14 10:09:31 +0100

    initial commit

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+    author = 'the user'
+    content = 'thecontent'
+    published = 'published date'
+    created = 'created date'
+
