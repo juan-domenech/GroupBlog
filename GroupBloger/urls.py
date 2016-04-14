dd202db56867c8eb5985b0da262e88ce8fcdc97a
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-04-14 10:58:30 +0100

    Initial vies for blog

--- GroupBloger/urls.py
+++ GroupBloger/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views as blog_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^blog/$', views.post_list),
+    url(r'^test/$', blog_views.test),
+    url(r'^blog/$', blog_views.post_list),
     # url(r'^blog/(?P<id>\d+)/$', views.post_details),
     # url(r'^post/new/$', views.new_post, name='new_post'),
 ]
